6abd9d2e298e0f73b5e2ff3c2892eae869bf1685
Damien Peskanoff <you@example.comprofitmargin999@gmail.com>
2022-11-03 21:21:51 -0400

    nucamp-react-site

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import { Container, Col, Row } from 'reactstrap';
 
 const Footer = () => {
     return (
-        <Footer ClassName='site-footer'>
+        <footer className='site-footer'>
             <Container>
                 <Row>
                     <Col xs={{ size: 4, offset: 1 }} sm='2'>
@@ -56,7 +56,7 @@ const Footer = () => {
                     </Col>
                 </Row>
             </Container>
-        </Footer>
+        </footer>
     )
 }
 
