070918594fadb0f52aed81cee6a8b47f7bb20cb5
Damien Peskanoff <you@example.comprofitmargin999@gmail.com>
2022-11-15 21:37:41 -0500

    React Router SPA

---
+++ src/pages/CampsiteDetailPage.js
@@ -0,0 +1,23 @@
+import { Container, Row } from 'reactstrap';
+import { useParams } from 'react-router-dom';
+import { selectCampsiteById } from '../features/counter/campsites/campsitesSlice';
+import CampsitesDetail from '../features/counter/campsites/CampsitesDetail';
+import CommentsList from '../features/counter/comments/CommentsList';
+import SubHeader from '../components/SubHeader';
+
+const CampsiteDetailPage = () => {
+    const { campsiteId} = useParams();
+    const campsite = selectCampsiteById(campsiteId);
+
+    return (
+        <Container>
+            <SubHeader current={campsite.name} detail={true} />
+            <Row>
+                <CampsitesDetail campsite={campsite} />
+                <CommentsList campsiteId={campsiteId} />
+            </Row>
+        </Container>
+    )
+}
+
+export default CampsiteDetailPage;
