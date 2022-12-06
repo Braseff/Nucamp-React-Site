100113c735c0b865aa0bbb2b8e9a32534508f04c
Dami Laptop <damidacoda@gmail.com>
2022-12-06 16:30:58 -0500

    redux and login form

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,18 @@
+import logger from 'redux-logger';
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { campsitesReducer } from '../features/counter/campsites/campsitesSlice';
+import { commentsReducer } from '../features/counter/comments/commentsSlice';
+import { partnersReducer } from '../features/counter/partners/partnersSlice';
+import { promotionsReducer } from '../features/counter/promotions/promotionsSlice';
+import { userReducer } from '../features/user/userSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    campsites: campsitesReducer,
+    comments: commentsReducer,
+    partners: partnersReducer,
+    promotions: promotionsReducer,
+    user: userReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger])
 });
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Navbar, NavbarBrand, Collapse, NavbarToggler, Nav, NavItem } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 import NucampLogo from '../app/assets/img/logo.png'
+import UserLoginForm from '../features/user/UserLoginForm';
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -35,8 +36,10 @@ const Header = () => {
                   <i className='fa fa-address-card fa-lg' /> Contact
                 </NavLink>
               </NavItem>
-
             </Nav>
+
+            <UserLoginForm></UserLoginForm>
+
           </Collapse>
       </Navbar>
     );
